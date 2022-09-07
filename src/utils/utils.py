1eac8c04c63066aae92a04631194cc839a61c8cb
renanDidier <rdr@cesar.school>
2022-09-06 21:57:47 -0300

    Update compare_models method to resample with replacement. Pass a copy of the test x and y while comparing models to not contamine the tests.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -107,38 +107,29 @@ def five_two(reg1, reg2, X, y, metric='default'):
   print("t_value for the current test is %.6f" % t_bar)
 
 
-def chunks(lst, chunk_size):
-  return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
-
 
 def compare_models(reg1, reg2, X, y, metric='default'):
-
-  # Initialize the score difference for the 1st fold of the 1st iteration 
-  p_1_1 = 0.0
-
-  # Initialize a place holder for the variance estimate
-  s_sqr = 0.0
 
   # Initialize scores list for both classifiers
   scores_1 = []
   scores_2 = []
   diff_scores = []
 
-  chunk_size = 8
-  data_x = chunks(X, chunk_size)[:-1]
-  #data_x = data_x[:-2] + [data_x[-2]+data_x[-1]]
-  data_y = chunks(y, chunk_size)[:-1]
-  #data_y = data_y[:-2] + [data_y[-2]+data_y[-1]]
+  X['solubility'] = y['solubility']
+  
+  random_state_list = [
+    4, 10, 33, 42, 22312,
+    400, 77, 809, 9, 111
+  ]
+  
+  # Iterate through samples
+  for i in range(len(random_state_list)):
+    
+    sample = X.sample(n=len(X), replace=True, random_state=random_state_list[i])
+    #sample.to_csv('~/Desktop/sample{}'.format(i))
 
-
-  # Iterate through chunks
-  for i_s in range(len(data_x)):
-
-    # Initialize score differences
-    p_i = np.zeros(2)
-
-    val_x = data_x[i_s]
-    val_y = data_y[i_s]
+    val_y = sample['solubility']
+    val_x = sample.loc[:, sample.columns != 'solubility']
 
     # Compute scores
     preds_1 = reg1.predict(val_x)
@@ -147,13 +138,29 @@ def compare_models(reg1, reg2, X, y, metric='default'):
     preds_2 = reg2.predict(val_x)
     score_2 = r2_score(val_y, preds_2)
 
+    if metric == "adj_r2":
+      score_1 = base_train_adj_r2 = get_adj_r2(
+        n_observations=len(val_x) / 2,
+        n_independent_variables=val_x.shape[1],
+        r2_score = score_1
+      )
+
+      score_2 = base_train_adj_r2 = get_adj_r2(
+        n_observations=len(val_x) / 2,
+        n_independent_variables=val_x.shape[1],
+        r2_score = score_2
+      )
+
 
     # keep score history for mean and stdev calculation
     scores_1.append(score_1)
     scores_2.append(score_2)
     diff_scores.append(score_1 - score_2)
-    print("Iteration %2d score difference = %.6f" % (i_s + 1, score_1 - score_2))
+    print("Iteration %2d score difference = %.6f" % (i + 1, score_1 - score_2))
 
   print(f"mean_score_1 {np.mean(scores_1)}, std {np.std(scores_1)}")
   print(f"mean_score_2 {np.mean(scores_2)}, std {np.std(scores_2)}")
   print(stats.ttest_rel(scores_1, scores_2))
+  a, b = stats.ttest_rel(scores_1, scores_2)
+  if b <= 0.05:
+    print("P value menor ou igual a 0.05")
